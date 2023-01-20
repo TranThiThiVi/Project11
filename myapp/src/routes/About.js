3452b30fb0713ac0da4c88793fbcdc4bd10835a1
Vii Thi <vi19042001@gmil.com>
2023-01-20 22:14:39 +0700

    Add new project

---
+++ myapp/src/routes/About.js
@@ -0,0 +1,25 @@
+import AboutUs from "../components/AboutUs";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import AboutImg from "../img/13.jpg";
+function About (){
+    return(
+       <>
+        <Navbar />
+      <Hero
+        cName="hero-mid"
+        heroImg={AboutImg}
+        title="About"
+        
+   
+     
+      btnClass="hide"
+      />
+      <AboutUs/>
+      <Footer/>
+       </>
+    )
+   }
+   export default About;
+// 1h50
